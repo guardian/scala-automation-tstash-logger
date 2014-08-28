351d49bb58ecb9b3f66dd270309f649deebb46f6
istvanpamer <istvan.pamer@guardian.co.uk>
2014-08-28 16:56:18 +0100

    removed unused imports

--- src/main/scala/com/gu/support/tstash/TstashAppender.scala
+++ src/main/scala/com/gu/support/tstash/TstashAppender.scala
@@ -2,16 +2,12 @@ package com.gu.automation.api
 
 import java.io.File
 
+import scala.collection.mutable
+
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.UnsynchronizedAppenderBase
 import com.gu.support.tstash.HttpClient
-import com.ning.http.client._
 import com.ning.http.client.websocket.{WebSocket, WebSocketTextListener, WebSocketUpgradeHandler}
-
-import scala.collection.mutable
-import scala.concurrent.{Promise, Await, Future}
-import scala.util.{Failure, Success}
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object TstashAppender {
   val sockets = mutable.Map[String, WebSocket]()
@@ -22,8 +18,6 @@ class TstashAppender extends UnsynchronizedAppenderBase[ILoggingEvent] {
   override def append(eventObject: ILoggingEvent): Unit = {
     val failed = """\[FAILED\](.*)""".r
 
-    // TODO: send proper screenshots
-    // TODO: store messages for tests
     eventObject.getMessage match {
       case "[TEST START]" => createWebSocket(eventObject).map(TstashAppender.sockets.put(eventObject.getMDCPropertyMap.get("ID"), _))
       case "[TEST END]" => TstashAppender.sockets.get(eventObject.getMDCPropertyMap.get("ID")).map(_.close())
