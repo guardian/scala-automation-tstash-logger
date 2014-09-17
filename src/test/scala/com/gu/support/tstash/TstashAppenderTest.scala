548cf9f4d1747c4da7ca0f5103075523056d623c
istvanpamer <istvan.pamer@guardian.co.uk>
2014-09-17 15:58:43 +0100

    removing webdriver dependent test

--- src/test/scala/com/gu/support/tstash/TstashAppenderTest.scala
+++ src/test/scala/com/gu/support/tstash/TstashAppenderTest.scala
@@ -28,27 +28,27 @@ class TstashAppenderTest extends FlatSpec with Matchers with LazyLogging {
 
   }
 
-  "The auth api 222" should "let us log in as a valid 222 user" in {
-
-    sys.props.put("teststash.url", "localhost:9000")
-    MDC.put("ID", UUID.randomUUID().toString)
-    MDC.put("testName", "test name 2")
-    MDC.put("testDate", DateTime.now.getMillis.toString)
-    MDC.put("setName", "set name 1")
-    MDC.put("setDate", SetTime.time.getMillis.toString)
-    println("setDate: " + SetTime.time.getMillis.toString)
-
-    logger.info("[TEST START]")
-    logger.info("Test message 1111.")
-    logger.info("Test message 2222222.")
-    val driver = new FirefoxDriver()
-    driver.get("http://www.google.com")
-    logger.info("[FAILED]Element not found on page.")
-    logger.info("[SCREENSHOT]", driver.getScreenshotAs(OutputType.BYTES))
-    driver.quit()
-    logger.info("[TEST END]")
-
-  }
+//  "The auth api 222" should "let us log in as a valid 222 user" in {
+//
+//    sys.props.put("teststash.url", "localhost:9000")
+//    MDC.put("ID", UUID.randomUUID().toString)
+//    MDC.put("testName", "test name 2")
+//    MDC.put("testDate", DateTime.now.getMillis.toString)
+//    MDC.put("setName", "set name 1")
+//    MDC.put("setDate", SetTime.time.getMillis.toString)
+//    println("setDate: " + SetTime.time.getMillis.toString)
+//
+//    logger.info("[TEST START]")
+//    logger.info("Test message 1111.")
+//    logger.info("Test message 2222222.")
+//    val driver = new FirefoxDriver()
+//    driver.get("http://www.google.com")
+//    logger.info("[FAILED]Element not found on page.")
+//    logger.info("[SCREENSHOT]", driver.getScreenshotAs(OutputType.BYTES))
+//    driver.quit()
+//    logger.info("[TEST END]")
+//
+//  }
 
 }
 
