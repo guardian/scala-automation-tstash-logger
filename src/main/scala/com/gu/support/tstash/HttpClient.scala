532651be684a3f8ece399774eaa4bc2fbba299c6
jharewinton <jonathan.hare-Winton@guardian.co.uk>
2015-01-21 16:28:32 +0000

    Remove unnecessary 'http://' from HttpClient

--- src/main/scala/com/gu/support/tstash/HttpClient.scala
+++ src/main/scala/com/gu/support/tstash/HttpClient.scala
@@ -7,8 +7,8 @@ import com.ning.http.client.{AsyncHttpClient, AsyncHttpClientConfig}
  */
 object HttpClient {
 
-  lazy val urlReport = s"http://${sys.props.getOrElse("teststash.url", "NOTSET")}/report"
-  lazy val urlSS = s"http://${sys.props.getOrElse("teststash.url", "NOTSET")}/screenShotUpload"
+  lazy val urlReport = s"${sys.props.getOrElse("teststash.url", "NOTSET")}/report"
+  lazy val urlSS = s"${sys.props.getOrElse("teststash.url", "NOTSET")}/screenShotUpload"
 
   private lazy val config = new AsyncHttpClientConfig.Builder()
   lazy val httpClient = new AsyncHttpClient(config.build())
